01a498ef4f83dd01fba3a1414a88b4e230caacf8
Abdullah <abdullah.farhan.uet@gmail.com>
2025-02-01 16:06:55 +0500

    server.js altered

--- Backend/server.js
+++ Backend/server.js
@@ -18,10 +18,10 @@ const __dirname = path.resolve();
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../Frontend/dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
   });
 }
 
