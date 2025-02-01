8a1a216b9c33b7459f04bd6171a452ca7593c9d1
Abdullah <abdullah.farhan.uet@gmail.com>
2025-02-01 16:21:21 +0500

    settings

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // Ensure the output is in 'dist'
-  },
-  server: {
-    port: 3000,
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
